bcd907240d8735f18458fc37ce49c133092f4abf
NeZlox <cnegovek@mail.ru>
2024-10-30 22:57:52 +0300

    создание схемы ответа от сервиса ParseService

--- src/api/parse_images/router.py
+++ src/api/parse_images/router.py
@@ -28,7 +28,11 @@ async def start_parsing(
         query_params: StartParsingRequestSchema = Depends(StartParsingRequestSchema)
 ):
     """Запускает асинхронный парсер для обработки изображений."""
-    if query_params.start_id is not None and query_params.end_id is not None and query_params.start_id > query_params.end_id:
+    if (
+            query_params.start_id is not None
+            and query_params.end_id is not None
+            and query_params.start_id > query_params.end_id
+    ):
         raise HTTPException(status_code=400, detail="start_id должен быть меньше end_id")
 
     try:
